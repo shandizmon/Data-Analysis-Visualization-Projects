4625fd495fe9b14f9aa0d2ee96c60381758ce6d1
shandizmon <37096259+shandizmon@users.noreply.github.com>
2018-06-18 22:46:25 -0700

    Last version WeatherPy assignment #6

--- 06-Python-APIs/WeatherPy.py
+++ 06-Python-APIs/WeatherPy.py
@@ -1,7 +1,18 @@
 
 # coding: utf-8
 
-# In[5]:
+# # Analysis
+# 
+# ### Observed Trend 1:
+# Cities loacated close to latitude 0 (equator) have the highest temprature and as you get farther from    equator temperature decreases. Please note that these tempretaure are collected in the hottest period of the year and conclusively the temperature range is less than if we had collected data in winter or spring.
+# 
+# ### Observed Trend 2: 
+# Cities loacated close to latitude 0 (equator) have a higher humidity which is probably due to higher water evaporation rate. Also it looks for the cities in the southern hemisphere the farthest ones from equator have higher humidity.
+# 
+# ### Observed Trend 3: 
+# Laditude does not seem to have a correlation to wind speed and cloudiness.
+
+# In[1]:
 
 
 # Dependencies
@@ -17,23 +28,29 @@ import matplotlib.pyplot as plt
 from apikeys import api_key as gkey
 
 
-# In[6]:
+# In[2]:
 
 
 # geocoordinates
 
-def newpoint():
-    return uniform(-90.0,90.0), uniform(-180.0,180.0)
+def newpoint1():
+    return uniform(-90.0,0), uniform(-180.0,180.0)
+
+def newpoint2():
+    return uniform(0,90), uniform(-180.0,180.0)
 
 coordinates = []
-points = (newpoint() for x in range(10))
+points = (newpoint1() for x in range(300))
+for point in points:
+    coordinates.append(point)
+
+points = (newpoint2() for x in range(300))
 for point in points:
     coordinates.append(point)
     
-coordinates
 
 
-# In[7]:
+# In[3]:
 
 
 cities = []
@@ -41,10 +58,9 @@ for coordinate_pair in coordinates:
     lat, lon = coordinate_pair
     cities.append((citipy.nearest_city(lat, lon).city_name))
     
-cities
 
 
-# In[8]:
+# In[4]:
 
 
 # list for response results
@@ -64,37 +80,123 @@ base_url = "http://api.openweathermap.org/data/2.5/weather"
 
 # Get weather information in JSON format
 for city in cities:
-#    print(city)
-    api_params['q']=city
-    weather_response = requests.get(base_url, params=api_params).json()
+    # Adding "null" value for the cities which don't have value by "try" and "except"
+    try:
+        api_params['q']=city
+        weather_response = requests.get(base_url, params=api_params).json()
     
-    lat.append(weather_response['coord']['lat'])
-    temp.append(weather_response['main']['temp'])
-    humidity.append(weather_response['main']['humidity'])
-    clouds.append(weather_response['clouds']['all'])
-    wind.append(weather_response['wind']['speed'])
+        lat.append(weather_response['coord']['lat'])
+        temp.append(weather_response['main']['temp_max'])
+        humidity.append(weather_response['main']['humidity'])
+        clouds.append(weather_response['clouds']['all'])
+        wind.append(weather_response['wind']['speed'])
+    except:
+        lat.append('null')
+        temp.append('null')
+        humidity.append('null')
+        clouds.append('null')
+        wind.append('null')
+
 
 
 # build a dataframe from the cities, lat, Temperature, Humidity (%), Cloudiness (%),Wind Speed (mph) lists
-weather_data = {"City": cities, "Latitude": lat, "Temperature (F)": temp, "Humidity (%)": humidity, "Cloudiness (%)": clouds, "Wind Speed (mph)": wind}
+weather_data = {"City": cities, "Latitude": lat, "Max Temperature (F)": temp, "Humidity (%)": humidity, 
+                "Cloudiness (%)": clouds, "Wind Speed (mph)": wind}
 df_weather_data = pd.DataFrame(weather_data)
 df_weather_data.head()
 
 
-# In[11]:
+# In[5]:
 
 
-# Build a scatter plot for each data type
-plt.scatter(df_weather_data["Latitude"], df_weather_data["Temperature (F)"], marker="o")
+# Removing the null value rows from the dataframe
+df_weather_data = df_weather_data[df_weather_data.Latitude!='null']
+df_weather_data['City'].count()
+
+
+# In[6]:
+
+
+# Build the Temperature scatter plot for dataframe
+plt.figure(figsize= (12,5))
+plt.scatter(df_weather_data["Latitude"], df_weather_data["Max Temperature (F)"], marker="o")
 
 # Incorporate the other graph properties
-plt.title("Temperature vs. Latitude in World Cities")
-plt.ylabel("Temperature (Fahrenheit)")
+plt.ylim(-100,150)
+plt.xlim(-80,100)
+plt.title("City Latitude vs. Max Temperature (06/18/2018)")
+plt.ylabel("Max Temperature (Fahrenheit)")
 plt.xlabel("Latitude")
 plt.grid(True)
 
 # Save the figure
-plt.savefig("TemperatureInWorldCities.png")
+plt.savefig("CityLatitudevsMaxTemperature.png")
+
+# Show plot
+plt.show()
+
+
+# In[7]:
+
+
+# Build the Humidity scatter plot for dataframe
+plt.figure(figsize= (12,5))
+plt.scatter(df_weather_data["Latitude"], df_weather_data["Humidity (%)"], marker="o")
+
+# Incorporate the other graph properties
+plt.ylim(-20,120)
+plt.xlim(-80,100)
+plt.title("City Latitude vs. Humidity (06/18/2018)")
+plt.ylabel("Humidity (%)")
+plt.xlabel("Latitude")
+plt.grid(True)
+
+# Save the figure
+plt.savefig("CityLatitudevsHumidity.png")
+
+# Show plot
+plt.show()
+
+
+# In[8]:
+
+
+# Build the Cloudiness scatter plot for dataframe
+plt.figure(figsize= (12,5))
+plt.scatter(df_weather_data["Latitude"], df_weather_data["Cloudiness (%)"], marker="o")
+
+# Incorporate the other graph properties
+plt.ylim(-20,120)
+plt.xlim(-80,100)
+plt.title("City Latitude vs. Cloudiness (06/18/2018)")
+plt.ylabel("Cloudiness (%)")
+plt.xlabel("Latitude")
+plt.grid(True)
+
+# Save the figure
+plt.savefig("CityLatitudevsCloudiness.png")
+
+# Show plot
+plt.show()
+
+
+# In[9]:
+
+
+# Build the Wind Speed scatter plot for dataframe
+plt.figure(figsize= (12,5))
+plt.scatter(df_weather_data["Latitude"], df_weather_data["Wind Speed (mph)"], marker="o")
+
+# Incorporate the other graph properties
+plt.ylim(-5,40)
+plt.xlim(-80,100)
+plt.title("City Latitude vs. Wind Speed (06/18/2018)")
+plt.ylabel("Wind Speed (mph)")
+plt.xlabel("Latitude")
+plt.grid(True)
+
+# Save the figure
+plt.savefig("CityLatitudevsWindSpeed.png")
 
 # Show plot
 plt.show()
