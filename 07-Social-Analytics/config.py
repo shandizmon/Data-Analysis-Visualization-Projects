218bbdf04e0357c562e57e3f41ae9ce29e2879f6
shandizmon <37096259+shandizmon@users.noreply.github.com>
2018-06-27 00:03:14 -0700

    Final homework#7 Social-Analytics- News mood

---
+++ 07-Social-Analytics/config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "IyCaRJ3fErXreU5pFhBfVxaiB"
+consumer_secret = "EzwziOQUN8SMcN68kXiSjP42TtpPx2JhMYq8qbsAnMRs0kiBix"
+access_token = "1007436614635741185-k0xBm2FBziqsLkz7G5cUh5XiXzob5N"
+access_token_secret = "q4xDON7xGoCKBwWB1E50bIRsB4lLQ5JCrMVOFzILL9aMr"
