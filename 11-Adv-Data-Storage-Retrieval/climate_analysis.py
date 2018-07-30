38120cc69689b0c4924626ec48a061961985aea5
shandizmon <37096259+shandizmon@users.noreply.github.com>
2018-07-30 11:13:27 -0700

    Almost done assingnment 11

--- 11-Adv-Data-Storage-Retrieval/climate_analysis.py
+++ 11-Adv-Data-Storage-Retrieval/climate_analysis.py
@@ -126,3 +126,110 @@ df_date_precipitation.describe()
 # * Filter by the station with the highest number of observations.
 # 
 # * Plot the results as a histogram with `bins=12`.
+
+# In[8]:
+
+
+# Calculate the total number of stations 
+station_count = session.query(func.count(Station.station)).all()
+station_count[0]
+
+
+# In[9]:
+
+
+# Find and list the most active stations
+(session.query(Measurement.station, Station.name, func.count(Measurement.station))
+ .filter(Measurement.station==Station.station)
+ .group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all())
+
+
+# In[10]:
+
+
+# Find the station has the highest number of observations
+(session.query(Measurement.station, Station.name, func.count(Measurement.station))
+ .filter(Measurement.station==Station.station)
+ .group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).limit(1).all())
+
+
+# In[31]:
+
+
+date_tobs=(session.query(Measurement.date,Measurement.tobs)
+.filter(Measurement.station=='USC00519281')
+.filter(Measurement.date>=start_date)
+.order_by(Measurement.date.desc()).all())
+
+
+# In[38]:
+
+
+# Stations with the highest number of observations
+tobs_df=pd.DataFrame(date_tobs, columns=['date','tobs'])
+tobs_df.head()
+
+
+# In[33]:
+
+
+# Plot the results as a histogram with bins=12.
+tobs_df.plot(kind='hist',bins=12, figsize=(15,10))
+plt.title("Temperature Observation Data")
+plt.savefig("tobs.png")
+plt.show()
+
+
+# ##### Temperature Analysis
+# 
+# * Write a function called `calc_temps` that will accept a start date and end date in the format `%Y-%m-%d` and return the minimum, average, and maximum temperatures for that range of dates.
+# 
+# * Use the `calc_temps` function to calculate the min, avg, and max temperatures for your trip using the matching dates from the previous year (i.e. use "2017-01-01" if your trip start date was "2018-01-01")
+# 
+# * Plot the min, avg, and max temperature from your previous query as a bar chart.
+# 
+# * Use the average temperature as the bar height.
+# 
+# * Use the peak-to-peak (tmax-tmin) value as the y error bar (yerr).
+
+# In[34]:
+
+
+#Write a function called calc_temps
+def calc_temps(start_date, end_date):
+    temp=(session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs))
+              .filter(Measurement.date>=start_date)
+              .filter(Measurement.date<=end_date).all())
+   
+    return temp
+
+
+# In[40]:
+
+
+# Use the calc_temps function to calculate the min, avg, and max temperatures
+match_calc_temp=calc_temps('2017-05-05', '2017-05-18')
+print(match_calc_temp)
+type(match_calc_temp)
+
+
+# In[41]:
+
+
+# Plot the min, avg, and max temperature from your previous query as a bar chart.
+x=1
+y=round(match_calc_temp[0][1],2)
+yerr=match_calc_temp[0][2]-match_calc_temp[0][0]
+
+
+# In[43]:
+
+
+# Create the plot
+fig = plt.figure(figsize=(4, 8))
+plt.bar(x, y, yerr=yerr, align='center', color='pink', alpha=0.7, tick_label=1)
+plt.title("Trip Avg Temp")
+plt.ylabel("Temp(F)")
+plt.savefig("bar.png")
+plt.show()
+
